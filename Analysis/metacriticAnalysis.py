b3b74d1e65a5c143348513a8475a2aa480b1d77e
LukasMaselsky <lukasmaselsky@gmail.com>
2024-07-16 21:27:08 +0100

    paths

--- Analysis/metacriticAnalysis.py
+++ Analysis/metacriticAnalysis.py
@@ -2,19 +2,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statistics
 from datetime import datetime
+from textwrap import wrap
+import os
 
-df = pd.read_csv('mergedMetacritic.csv')
+# path of the given file
+print(os.path.dirname(os.path.abspath("mergedMetacritic.csv")))
+
+
+df = pd.read_csv(r'Analysis/mergedMetacritic.csv')
 df = df.dropna()
 
-df['date'] = pd.to_datetime(df['date'], format="%Y/%m/%d")
+df['date'] = pd.to_datetime(df['date'], format="ISO8601")
 df['year'] = pd.DatetimeIndex(df['date']).year
 
 print(df['year'].value_counts()) # shows 96-99 are outliers due to the low amount of games those years
 print(df['developer'].value_counts()) # most common developers
 print(df['rating'].value_counts())
 print(df['platform'].value_counts())
-print(df['genres'].str.split(',\s+', expand=True).stack().value_counts()) #* BUG??
-
+print(df['genres'].str.split(',\s+', expand=True).stack().value_counts()) 
 # filter out 'ratings' string
 df['numberofuserreviews'] = df['numberofuserreviews'].str.extract('(\d+)').astype(int)
 
@@ -39,6 +44,10 @@ def weighted_rating(x, m, c, t):
         # Calculation based on the IMDB formula
         return (v/(v+m) * r) + (m/(m+v) * c)
 
+
+
+
+
 df['userweighted'] = df.apply(weighted_rating, args=(1, df['userscore'].mean(), 'user'), axis=1)
 df['criticweighted'] = df.apply(weighted_rating, args=(1, df['metascore'].mean(), 'critic'), axis=1)
 
@@ -47,13 +56,12 @@ df['overallweighted'] = df[['userweighted', 'criticweighted']].mean(axis=1)
 df.sort_values(by=['overallweighted'], ascending=False, inplace=True)
 
 years = sorted(df['year'].unique()) # all possible year values
-
-overall = list(df.groupby('year')['overallweighted'].apply(list).values) # list of weighted scores by year
+overallYears = list(df.groupby('year')['overallweighted'].apply(list).values) # list of weighted scores by year
 
 # iterates over overall and gets mean weighted average for each year
 final = []
-for list in overall:
-    final.append(statistics.fmean(list))
+for l in overallYears:
+    final.append(statistics.fmean(l))
 
 for i, year in enumerate(years):
     years[i] = str(year)[-2:]
@@ -65,4 +73,29 @@ plt.yticks([i for i in range(0, 101, 5)])
 plt.title('Average score of a video game each year')
 plt.xlabel('Years (1996-2023)')
 plt.ylabel('Average score')
+plt.show()
+
+
+
+developers = sorted(df['developer'].unique())
+overallDevelopers = list(df.groupby('developer')['overallweighted'].apply(list).values)
+
+
+final = []
+for l in overallDevelopers:
+    final.append(statistics.fmean(l))
+
+topDevs = list(df['developer'].value_counts().index[:20]) # top 20 developers by COUNT
+topDevsDict = {}
+
+for dev in topDevs:
+    index = developers.index(dev)
+    topDevsDict[dev] = final[index] 
+    #changed.append(final[index])
+
+
+topDevsDict = {k: v for k, v in reversed(sorted(topDevsDict.items(), key=lambda item: item[1]))} # sort 
+topDevs = ["\n".join(wrap(dev, 5)) for dev in topDevsDict.keys()]
+
+plt.bar(topDevs, topDevsDict.values()) #! fix label issue/legend      
 plt.show()
